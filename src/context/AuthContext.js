84cefb47978e74905dfba5688e94d479617154cb
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-24 19:28:56 -0800

    Finish setting up JWT token and get api data

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -11,6 +11,7 @@ export const AuthProvider = ({children}) => {
     
     let [authTokens, setAuthTokens] = useState(() => localStorage.getItem('authTokens') ? JSON.parse(localStorage.getItem('authTokens')) : null);
     let [user, setUser] = useState(() => localStorage.getItem('authTokens') ? jwt_decode(localStorage.getItem('authTokens')) : null);
+    let [loading, setLoading] = useState(true);
 
     const navigate = useNavigate();
 
@@ -41,14 +42,55 @@ export const AuthProvider = ({children}) => {
         navigate('/login');
     }
 
+    let updateToken = async () => {
+        console.log("Update token called")
+        let response = await fetch('http://127.0.0.1:8000/api/v1/token/refresh/', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({'refresh': authTokens?.refresh})
+        });
+        let data = await response.json();
+
+        if (response.status === 200) {
+            setAuthTokens(data);
+            setUser(jwt_decode(data.access));
+            localStorage.setItem('authTokens', JSON.stringify(data));
+        } else {
+            logoutUser();
+        }
+
+        if (loading) {
+            setLoading(false);
+        };
+    }
+
     let contextData = {
         user:user,
+        authTokens:authTokens,
         loginUser:loginUser,
         logoutUser:logoutUser
     }
+
+    useEffect(() => {
+
+        if(loading) {
+            updateToken();
+        }
+        let fourMinutes = 1000*60*4;
+        let interval = setInterval(() => {
+            if(authTokens) {
+                updateToken()
+            }
+        }, fourMinutes)
+        return () => clearInterval(interval) 
+
+    }, [authTokens, loading])
+
     return(
         <AuthContext.Provider value={contextData}>
-            {children}
+            {loading ? null : children}
         </AuthContext.Provider>
     )
 }
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,43 @@
+import { useContext, useEffect, useState } from "react";
+import AuthContext from "../context/AuthContext";
+
 function HomePage() {
+    let [posts, setPosts] = useState([]);
+    let {authTokens, logoutUser} = useContext(AuthContext);
+
+    useEffect(() => {
+        getPosts()
+    },[])
+
+    let getPosts = async () => {
+        let response = await fetch('http://127.0.0.1:8000/api/v1/', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + String(authTokens.access) 
+            }
+        })
+        let data = await response.json()
+
+        if (response.status === 200) {
+            setPosts(data)
+        } else if (response.statusText === 'Unauthorized') {
+            logoutUser();
+        };
+        
+    }
 
     return (
         <div>
+            <p>You are logged into the home page!</p>
+                {posts.map(post => {
+                    return (
+                        <div key={post.title}>
+                            <h2 key={post.title}>{post.title}</h2>
+                            <img src={post.image} alt={post.title} />
+                        </div>
+                    )
+                })}
         </div>
     )
     }
