094b2125ea25de6949de7ff8570445c0c86ce221
MichaelABryant <m.alexander.bryant@gmail.com>
2023-02-26 19:00:53 -0800

    Add comments to post detail

--- src/pages/PostDetail.js
+++ src/pages/PostDetail.js
@@ -5,6 +5,7 @@ function PostDetail() {
     let {authTokens, user} = useContext(AuthContext);
 
     let [post, setPost] = useState([]);
+    let [comments, setComments] = useState([]);
 
     let currentUrl = window.location.href
     let postId = currentUrl.slice(currentUrl.indexOf("postdetail/")+11)
@@ -25,16 +26,38 @@ function PostDetail() {
         };
     }
 
+    let getComments = async () => {
+        let response = await fetch(`http://127.0.0.1:8000/api/v1/comments/${postId}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': 'Bearer ' + String(authTokens.access)
+            }
+        });
+        let data = await response.json()
+
+        if (response.status === 200) {
+            setComments(data)
+        } else if (response.statusText === 'Unauthorized') {
+            console.log("unauthorized")
+        };
+    }
+
     
 
     useEffect(() => {
         getPost();
+        getComments();
     }, [])
 
     return (
     <div id="postdetail-container">
         <h2>{post.title}</h2>
         <img className="img-detail" src={post.image} alt={post.title}/>
+        <div className="comments">
+        {comments.map(comment => {
+           return (<div><h3>{comment.author}</h3><p>{comment.comment}</p></div>)
+        })}
+        </div>
     </div>)
 }
 
